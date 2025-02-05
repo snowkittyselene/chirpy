1c725e4e3b77addabe17763c76d8760cb210a5cb
Selene Moonsilver <bobdablob42+gamedev@gmail.com>
2025-02-05 18:31:54 -0500

    Add HTTP server

--- main.go
+++ main.go
@@ -1,5 +1,18 @@
 package main
 
-func main() {
+import (
+	"log"
+	"net/http"
+)
 
+const PORT = "8080"
+
+func main() {
+	mux := http.NewServeMux()
+	srv := http.Server{
+		Addr:    ":" + PORT,
+		Handler: mux,
+	}
+	log.Printf("Server running on port: %s", PORT)
+	log.Fatal(srv.ListenAndServe())
 }
