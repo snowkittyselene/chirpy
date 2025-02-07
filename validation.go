28b2339b981f620ca997837362d7e9d40cfa2f2d
Selene Moonsilver <bobdablob42+gamedev@gmail.com>
2025-02-07 01:15:22 -0500

    Add bad word removal

--- validation.go
+++ validation.go
@@ -3,14 +3,18 @@ package main
 import (
 	"encoding/json"
 	"net/http"
+	"slices"
+	"strings"
 )
+
+var badWords = []string{"kerfuffle", "sharbert", "fornax"}
 
 func handlerValidation(w http.ResponseWriter, r *http.Request) {
 	type chirp struct {
 		Body string `json:"body"`
 	}
 	type validResponse struct {
-		Valid bool `json:"valid"`
+		CleanedBody string `json:"cleaned_body"`
 	}
 
 	decoder := json.NewDecoder(r.Body)
@@ -23,9 +27,22 @@ func handlerValidation(w http.ResponseWriter, r *http.Request) {
 		respondError(w, http.StatusBadRequest, "Chirp is too long", nil)
 	} else {
 		successMessage := validResponse{
-			Valid: true,
+			CleanedBody: removeBadWords(sentChirp.Body),
 		}
 		respondWithJSON(w, http.StatusOK, successMessage)
 	}
 
+}
+
+func removeBadWords(original string) string {
+	goodWords := []string{}
+	words := strings.Fields(original)
+	for _, word := range words {
+		if slices.Contains(badWords, strings.ToLower(word)) {
+			goodWords = append(goodWords, "****")
+		} else {
+			goodWords = append(goodWords, word)
+		}
+	}
+	return strings.Join(goodWords, " ")
 }
